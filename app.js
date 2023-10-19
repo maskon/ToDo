a9f03bed2bde5edb79f4f23265c6966ebe117fa8
Максим Блинов <126991331+maskon@users.noreply.github.com>
2023-10-19 14:06:03 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const inputElement = document.querySelector('#title')
 const buttonElement = document.querySelector('#btn')
 const listElement = document.querySelector('#list')
+const textErrorElement = document.querySelector('#text-error')
 
 const array = [
 //    {
@@ -14,6 +15,7 @@ const array = [
 
 function render() {
     listElement.innerHTML = ''
+    textErrorElement.innerHTML = ''
     
     if (array.length === 0) {
         listElement.innerHTML = `<p>Нет ни одной задачи</p>`
@@ -29,7 +31,22 @@ render()
 buttonElement.addEventListener('click', function() {
     if (inputElement.value.match(/^\s*$/)) {
         inputElement.focus()
-        listElement.innerHTML = `<p class="text-red">Ошибка! Поле не может быть пустым!</p>`
+        textErrorElement.innerHTML = `<p class="text-red">Ошибка! Поле не может быть пустым!</p>`
+        
+        if (array.length === 0) {
+            listElement.innerHTML = ''
+        }
+        return
+    }
+    if (inputElement.value.match(/["№;%:?*()_\-+=\/.,`!@#$^&|]/)) {
+        inputElement.focus()
+        textErrorElement.innerHTML = `
+            <p class="text-red">Ошибка! Поле содержит запрещенные символы!</p>
+            <p class="text-red">Разрешены только буквы и цифры</p>`
+        
+        if (array.length === 0) {
+            listElement.innerHTML = ''
+        }
         return
     }
     
@@ -62,7 +79,7 @@ listElement.addEventListener('click', function(e) {
 function getNotTemplate(note, index) {
     return `
         <li
-          class="list-group-item d-flex justify-content-between align-items-center"
+          class="list-group-item d-flex justify-content-between align-items-center ${note.completed ? 'li-background' : ''}"
         >
           <span class="${note.completed ? 'text-done' : ''}">${note.title}</span>
           <span>

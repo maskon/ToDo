61834f9f0860324fd1558b79551472089331d360
Максим Блинов <126991331+maskon@users.noreply.github.com>
2023-10-23 12:19:35 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,55 +3,71 @@ const buttonElement = document.querySelector('#btn')
 const listElement = document.querySelector('#list')
 const textErrorElement = document.querySelector('#text-error')
 
-let array = []
+let array = [] // Создаем массив
 
 // Проверяем есть ли сохраненные данные в localstorage
 if (localStorage.getItem('array')) {
     array = JSON.parse(localStorage.getItem('array'))
 }
 
+// Функция запускает цикл
 function render() {
-    listElement.innerHTML = ''
-    textErrorElement.innerHTML = ''
+    listElement.innerHTML = '' // Очищаем список задач
+    textErrorElement.innerHTML = '' // Очищаем поле с ошибками
     
+    // Проверка, что массив не пустой
     if (array.length === 0) {
         listElement.innerHTML = `<p class="text-16">Нет ни одной задачи</p>`
     }
     
+    // Цикл
     for (let i = 0; i < array.length; i++) {
         listElement.insertAdjacentHTML('beforeend', getNotTemplate(array[i], i))
     }
 }
 
-render()
+render() // Запускаем функцию
 
+// Функция (если массив пустой)
 function checkArrayEmpty() {
     if (array.length === 0) {
-        listElement.innerHTML = ''
+        listElement.innerHTML = '' // Очищаем список задач
     }
 }
 
+// Клик по кнопке
 buttonElement.addEventListener('click', function() {
-    let date = new Date().toISOString().slice(0,10).split('-').reverse().join('.')
-    let time = new Date().toLocaleTimeString()
+    let date = new Date().toISOString().slice(0,10).split('-').reverse().join('.') // Создаем дату
+    let time = new Date().toLocaleTimeString() // Создаем время
     
+    // Проверка если поле инпут не заполнено
     if (inputElement.value.match(/^\s*$/)) {
-        inputElement.focus()
+        inputElement.focus() // Фокус на поле инпут
         textErrorElement.innerHTML = `<p class="text-red">Ошибка! Поле не может быть пустым!</p>`
         
-        checkArrayEmpty()
-        return
+        checkArrayEmpty() // Запускаем функцию
+        return // Выходим из функции
     }
-    if (inputElement.value.match(/["№;%:?*()_\-+=\/.,`!@#$^&|]/)) {
-        inputElement.focus()
+    // Проверка поле инпут не содержит запрещеных символов
+    if (inputElement.value.match(/[№;%*_\+=\/`!@#$^&|]/)) {
+        inputElement.focus() // Фокус на поле инпут
         textErrorElement.innerHTML = `
-            <p class="text-red">Ошибка! Поле содержит запрещенные символы!</p>
-            <p class="text-red">Разрешены только буквы и цифры</p>`
+            <p class="text-red">Ошибка! Поле содержит запрещенные символы!</p>`
         
-        checkArrayEmpty()
-        return
+        checkArrayEmpty() // Запускаем функцию
+        return // Выходим из функции
     }
     
+    // Проверка на введеное кол-во символов
+    if (inputElement.value.length < 3) {
+        textErrorElement.innerHTML = `<p class="text-red">Введите не менее 3-х символов!</p>`
+        
+        checkArrayEmpty() // Запускаем функцию
+        inputElement.focus() // Фокус на поле инпут
+        return // Выходим из функции
+    }
+    
+    // Создаем объект
     const newItem = {
         title: inputElement.value,
         completed: false,
@@ -59,50 +75,55 @@ buttonElement.addEventListener('click', function() {
         time: time,
     }
     
-    array.push(newItem)
-    render()
-    inputElement.value = ''
-    inputElement.focus()
+    array.push(newItem) // Добавляем объект в массив
+    render() // Запускаем функцию
+    inputElement.value = '' // Очищаем поле инпут
+    inputElement.focus() // Фокус на поле инпут
     
-    localStorage.setItem('array', JSON.stringify(array))
+    localStorage.setItem('array', JSON.stringify(array)) // Записываем в localStorage
 })
 
 // Запускаем функцию по нажатию 'enter'
 inputElement.addEventListener('keypress', function (e) {
     let key = e.which || e.keyCode
-    if (key === 13) { // код клавиши Enter
-        buttonElement.click()
+    if (key === 13) { // Код клавиши Enter
+        buttonElement.click() // Запускаем функцию
     }
 })
 
+// Отслеживаем клик по кнопкам "выполнено" и "удалить"
 listElement.addEventListener('click', function(e) {
     if (e.target.dataset.index) {
-        const index = parseInt(e.target.dataset.index)
-        const type = e.target.dataset.type
+        const index = parseInt(e.target.dataset.index) // Получаем индекс кнопки
+        const type = e.target.dataset.type // Получаем нужную кнопку ('toggle' || 'remove')
         
         if (type === 'toggle') {
-            array[index].completed = !array[index].completed
+            array[index].completed = !array[index].completed // Кнопка "выполнено"
         } else if (type === 'remove') {
-            array.splice(index, 1)
-            inputElement.focus()
+            array.splice(index, 1) // Кнопка "удалить"
+            inputElement.focus() // Фокус на поле инпут
         }
         
-        localStorage.setItem('array', JSON.stringify(array))
-        render()
+        localStorage.setItem('array', JSON.stringify(array)) // Записываем в localStorage
+        render() // Запускаем функцию
     } 
 })
 
+// Функция создания html шаблона
 function getNotTemplate(note, index, date, time) {
     return `
         <li
           class="list-group-item d-flex justify-content-between align-items-center ${note.completed ? 'li-background' : ''}"
         >
-          <span class="${note.completed ? 'text-done' : ''}">${index < 9 ? 0 : ''}${index + 1}. ${note.title}</span>
-          <span class="${note.completed ? 'text-done' : ''} text--size">${note.date} ${note.time}</span>
+          <div>
+            <p class="${note.completed ? 'text-done' : ''}">${index < 9 ? 0 : ''}${index + 1}. ${note.title}</p>
+            <p class="${note.completed ? 'text-done' : ''} text--size">${note.date} ${note.time}</p>
+          </div>
           <span>
             <span class="btn btn-small btn--br btn-${note.completed ? 'warning' : 'success'}" data-index="${index}" data-type="toggle">&check;</span>
             <span class="btn btn-small btn-danger btn--br" data-index="${index}" data-type="remove">&times;</span>
           </span>
         </li>
+
     `
 }
